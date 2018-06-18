19586932694e7eb83b5b7df6943463adf7f55b73
Syd Vicious <cjschwichtenberg@gmail.com>
2018-06-18 17:39:43 -0400

    Fixed addButton/Search functionality and tried to figure out why gifs are displayig twice-could not find solution

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,12 @@
-$(document).ready(function(){
+
+    
+    // Inital array that become buttons immediately after document loaded
     var emotions = ["excited", "happy", "moody", "joyful", "facetious", "irate", "exhausted", "sad", "lonely", "thoughtful", "overzealous", "angry", "fearful", "surprise", "anxious"];
 
+    // Function that creats initial array buttons
     function renderButtons() {
         $("#button-view").empty();
-        for (var i = 0; i < emotions.length; i++){
+        for (var i = 0; i < emotions.length; i++) {
             var gifButton = $("<button>");
             gifButton.addClass("btn");
             gifButton.attr("id", emotions[i]);
@@ -12,68 +15,80 @@ $(document).ready(function(){
             $("#button-view").append(gifButton);
         }
     }
-    renderButtons();
 
-    $("#submit").on("click", function(event) {
-        event.preventDefault();
-        var emotion = $("#search-input").val().trim();
-        if (emotions.indexOf(emotion) == -1) {
-            emotions.push(emotion);
-            console.log(emotion);
-            console.log(emotions);
-        } else {
-            alert("This emotion has already been added!")
-        }
-        renderButtons();
-    })
-
-    $("button").on("click", function displayGif() {
-
-        $("#gif-view").empty();
+    // On click event for submit button that creates additional button and adds new search criteria to array
+    function addButtons() {
+        $("#submit").on("click", function(event) {
+            event.preventDefault();
+            var emotion = $("#search-input").val().trim();
+            if (emotions.indexOf(emotion) == -1) {
+                emotions.push(emotion);
+            } else if (emotion == "") {
+                return false;
+            } else {
+                alert("This emotion has already been added!");
+                return false;
+            }
+            renderButtons();
+        })
+    }   
     
-        emotions = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + emotions + "&api_key=6kGYVECnM4aCkSCUTSAYJZNBP3DW9aiH&limit=9";
+    // function that display's 10 gifs for initial buttons created
+    function displayGifs(event) {
+        // emptying div where displayed gif will appear, pulling emotion being called from array
+        $(".col-md-8").empty();
+        var emotionGif = $(this).attr("data-name");
 
+        // calling giphy API
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + emotionGif + "&api_key=QLqUXM8T40xKH2hVB3S8QwCIypOjqn5H&limit=9";
+        
         $.ajax({
             url: queryURL,
-            method: "GET"
-        }).done(function(response) {
-            var response = response.data
-            console.log(response);
-
-            for (var i = 0; i < response.length ; i++) {
-            
-                var title = response[i].title;
-                var rating = response[i].rating;                    var still = response[i].images.fixed_width_still.url;
-                var animated = response[i].images.fixed_width.url;
-
-                console.log(title);
-                console.log(rating);
-                console.log(still);
+            method: 'GET'
+        })
+        .done(function(response) {
+            $("#gifs-go-here").empty();
+            var results = response.data;
+        // for statement running through response data 9 times to display 9 gifs below
+            for (var i = 0; i < results.length; i++) {    
                 
-
+                // checkpoint and log to make sure ajax is returning response info- not needed for deploy
+                // var title = response[i].title;
+                // var rating = response[i].rating;                    
+                // var still = response[i].images.fixed_width_still.url;
+                // var animated = response[i].images.fixed_width.url;
+                // console.log(title);
+                // console.log(rating);
+                // console.log(still);
+            
+                // creating <div> with <img> and <p> elemets to display gifs, appending or prepending as necc. to div created in html index
                 var gifDiv = $("<div>").addClass("gif-div");
-                var gifImage = $("<img>").addClass("d-block").attr("src", response[i].images.fixed_width_still.url);
-                $(gifImage).attr("data-still", response[i].images.fixed_width_still.url);
-                $(gifImage).attr("data-animate", response[i].images.fixed_width.url);
-                $(gifImage).attr("data-current", "still")
-                var gifText = $("<p>").addClass("rating").text("Rating: " + response[i].rating);
+                var gifImage = $("<img>").addClass("d-block").attr("src", results[i].images.fixed_width_still.url);
+                $(gifImage).attr("data-still", results[i].images.fixed_width_still.url);
+                $(gifImage).attr("data-animate", results[i].images.fixed_width.url);
+                $(gifImage).attr("data-current", "still");
+                var gifText = $("<p>").addClass("rating").text("Rating: " + results[i].rating);
                 gifDiv.append(gifImage);
                 gifDiv.append(gifText);
                 $(".col-md-8").append(gifDiv);
-       
-             }   
-        })
-    });
+            }   
+        });
+    }   
+    
+    $(document).on("click", ":button", displayGifs);
 
+    renderButtons();
+
+    addButtons();
+
+    // on click event that plays/pauses individual gifs
     $(document).on("click", ".d-block", function(){
         var currentData = $(this).data("current");
         if (currentData == "still") {
             $(this).attr("src", $(this).data("animate"));
-            $(this).attr($(this).data("current", "animate"));
+            $(this).attr("data-current", "animate");
         } else {
             $(this).attr("src", $(this).data("still"));
-            $(this).attr($(this).data("current", "still"));
+            $(this).attr("data-current", "still");
         }
     });
-});
